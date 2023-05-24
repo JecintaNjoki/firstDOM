35e98e4399d9906120fb1903ee2c718f236a6454
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-05-24 17:51:28 +0300

    commit

--- index.js
+++ index.js
@@ -3,17 +3,30 @@ console.log({child});
 
 let lastChild=document.getElementById("container").lastChild
 console.log({lastChild});
- 
+// Change document background color to
+// silver 
 document.body.style.color="silver"
-
+// Change the font color for h1 title tag to
+// green
 document.getElementById("title").style.color="green";
-document.getElementById("title").style.textDecoration="";
+
+// Change the font case for h3 title tags to
+// uppercase
+document.getElementById("fruits").style.transform="uppercase";
+
+document.getElementById("vegies").style.transform="uppercase"
+
+// Add one more fruit to the fruits list
+let fruit =document.createElement("li");
+fruit.textContent="kiwi";
+document.getElementById("fruitList").appendChild(fruit);
 
 
-let =document.createElement("h2");
-fruit.innerHTML="kiwi";
-document.getElementById("fruitsList").appendChild(fruit);
-
+// Add one more vegetable to the vegetables
+// list
+let vegie=document.createElement("li");
+vegie.textContent="spinach";
+document.getElementById("vegList").appendChild(vegie);
 
 
 
