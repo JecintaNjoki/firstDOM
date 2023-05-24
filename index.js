86859377f5dc840fb28a77408d8615b4ded82d05
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-05-24 16:29:03 +0300

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let child=document.getElementById("container").children
+console.log({child});
+
+let lastChild=document.getElementById("container").lastChild
+console.log({lastChild});
+ 
+document.body.style.color="silver"
+
+document.getElementById("title").style.color="green";
+document.getElementById("title").style.textDecoration="";
+
+
+let =document.createElement("h2");
+fruit.innerHTML="kiwi";
+document.getElementById("fruitsList").appendChild(fruit);
+
+
+
+
